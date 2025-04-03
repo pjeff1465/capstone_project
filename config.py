f597605f25645f2fe7364f84b112e2b69bba7176
admin <admin@PP-Pro.local>
2025-04-03 17:39:21 -0500

    changed mealplan to db

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
     SECRET_KEY = 'secretkey'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///mealplan.sqlite'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite'
     SQLALCHEMY_TRACK_MODIFICATIONS = False 
