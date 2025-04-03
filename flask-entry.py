436330820f9919e69ec972b6f4687407af108f0b
admin <admin@PP-Pro.local>
2025-04-03 17:41:15 -0500

    added to main

--- flask-entry.py
+++ flask-entry.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from config import Config
 from extensions import db, login_manager
+from flask_sqlalchemy import SQLAlchemy 
 
 
 def create_app():
 
     app = Flask(__name__)
+    app.config.from_object(Config)
+    
+    db = SQLAlchemy(app)
 
     from . import routes
     app.register_blueprint(routes.bp)
