c86b28dc1fd889f2ae8b302c37b80b4332b84cce
admin <admin@PP-Pro.local>
2025-04-15 17:29:51 -0500

    added a bunch of stuff to login/signup page

--- flask-entry.py
+++ flask-entry.py
@@ -2,6 +2,7 @@ from flask import Flask, redirect, url_for
 from config import Config
 from extensions import db, login_manager
 from flask_sqlalchemy import SQLAlchemy 
+from flask_login import current_user
 
 def create_app():
     app = Flask(__name__)
@@ -22,7 +23,10 @@ app = create_app()
 
 @app.route('/')
 def home():
-    return redirect(url_for('auth.login'))
+    if current_user.is_authenticated: # flask_login feature that stores login state using cookie
+        return redirect(url_for('auth.dashboard')) # directs user that is already logged in to dashboard instead of sign up page
+    else:
+        return redirect(url_for('auth.signup'))
 
 if __name__ == '__main__':
     app.run(debug=True)
--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, logout_user, login_required, current_user
 from extensions import db, login_manager
@@ -14,18 +14,28 @@ def load_user(user_id):
 
 @auth.route('/signup', methods=['GET', 'POST'])
 def signup():
-    if request.method == 'POST':
+    if request.method == 'POST': # Save new login information to database
         email = request.form.get('email')
         password = request.form.get('password')
 
-        hashed_password = generate_password_hash(password)
-        new_user = User(email=email, password=hashed_password)
+        existing_user = User.query.filter_by(email=email).first() # check if user already exists
+        
+        if existing_user:
+            flash("Email already exists! Please log in.", "warning") # display warning message and redirect to log in page
+            return render_template('login.html')
+        
+        hashed_password = generate_password_hash(password) # hash password before saving (do not store raw password)
+        new_user = User(email=email, password=hashed_password) # create new user instance
 
+        # save user to database
         db.session.add(new_user)
         db.session.commit()
-        return redirect(url_for('auth.login'))
-    else:
+        
+# after storing user to DB redirect to login page. Allows to add email verifaction before automatically signing user in if wanted
+        return redirect(url_for('auth.login')) 
+    else: # its a GET request (user has not clicked sign up button)
         return render_template('signup.html')
+    
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -38,40 +48,19 @@ def login():
     # fetch user from database
     user = User.query.filter_by(email=email).first()
 
-    # check if passwrod correct by referencing db
+    # check if password correct by referencing db
     if user and check_password_hash(user.password, password): 
         login_user(user)
         return redirect(url_for('auth.dashboard'))
-
-    return 'Invalid email or password!'
+    else:
+        flash("Email or Password Incorrect! Please re-try.", "warning") # display warning message and redirect to log in page
+        return render_template('login.html')
 
 @auth.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('auth.login'))
-
-'''
-@login_manager.user_loader
-def user_loader(email):
-    if email not in login_user:
-        return
-
-    user = User()
-    user.id = email
-    return user
-
-
-@login_manager.request_loader
-def request_loader(request):
-    email = request.form.get('email')
-    if email not in login_user:
-        return
-
-    user = User()
-    user.id = email
-    return user
-'''
 
 @auth.route('/dashboard')
 @login_required
